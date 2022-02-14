04086bc6be77c3a4c47341bb97c9c7437c8058c9
Abel Antunez de la Fuente <abel.antunez@tink.com>
2022-02-14 10:37:33 +0100

    first commit

---
+++ src/main/java/com/ejemplo/springboot/web/app/EfacturaWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.ejemplo.springboot.web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfacturaWebAppApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EfacturaWebAppApplication.class, args);
+  }
+
+}
